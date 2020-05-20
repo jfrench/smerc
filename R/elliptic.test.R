5bf16fa765c483c23fe746e6fccd76261a3208a0
jpfrench81 <joshua.french@gmail.com>
2020-05-20 15:06:27 -0600

    Clean arg_check functions.

--- R/elliptic.test.R
+++ R/elliptic.test.R
@@ -130,3 +130,20 @@ elliptic.test = function(coords, cases, pop,
             d = NULL, a = a, shape_all = shape_all,
             angle_all = angle_all)
 }
+
+#' Additional argument checking for elliptic_test
+#'
+#' @param shape A vector of shapes (values >= 1)
+#' @param nangle A vector of angles for each shape (values >= 1)
+#' @param a A penalty parameter (a >= 0)
+#' @return NULL
+#' @noRd
+arg_check_elliptic_test =
+  function(shape, nangle, a) {
+    if (length(shape) != length(nangle)) {
+      stop("The length of shape and nangle must match.")
+    }
+    arg_check_shape(shape)
+    arg_check_nangle(nangle)
+    arg_check_a(a)
+  }
--- R/scan.test.R
+++ R/scan.test.R
@@ -178,3 +178,53 @@ scan.test = function(coords, cases, pop,
                 alpha = alpha,
                 w = NULL, d = d)
 }
+
+#' Argument checking for scan tests
+#'
+#' @param coords A matrix of coordinates
+#' @param cases A vector of numeric cases
+#' @param pop A vector of population values
+#' @param ex A vector of expected counts
+#' @param nsim A non-negative integer
+#' @param alpha A value greater than 0
+#' @param nreport Not used
+#' @param ubpop A value between 0 and 1
+#' @param longlat A logical. TRUE is great circle distance.
+#' @param parallel Not used.
+#' @param k Number of nearest neighbors. Not always needed.
+#' @param w A spatial proximity matrix
+#' @param type Statistic type
+#' @param simdist Distribution of simulation
+#' @param min.cases Minimum number of cases. Only for scan.test.
+#' @return NULL
+#' @noRd
+arg_check_scan_test =
+  function(coords, cases, pop, ex, nsim, alpha,
+           nreport = NULL,
+           ubpop, longlat, parallel = NULL, k, w, type = NULL,
+           simdist = NULL, min.cases = NULL) {
+  arg_check_coords(coords)
+  N = nrow(coords)
+  arg_check_cases(cases, N)
+  arg_check_pop(pop, N)
+  arg_check_ex(ex, N)
+  arg_check_nsim(nsim)
+  arg_check_alpha(alpha)
+  # nreport no check, deprecated
+  arg_check_ubpop(ubpop)
+  arg_check_longlat(longlat)
+  # parallel no check, deprecated
+  arg_check_k(k, N)
+  arg_check_w(w, N)
+  if (!is.null(type)) {
+    arg_check_type(type)
+  }
+  if (!is.null(simdist)) {
+    arg_check_simdist(simdist)
+  }
+  arg_check_simdist(simdist)
+  if (!is.null(min.cases)) {
+    arg_check_min_cases(min.cases)
+  }
+}
+
