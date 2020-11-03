41ea2e16ad1087c2b746c754b636b5f1a86febae
jpfrench81 <joshua.french@gmail.com>
2020-11-03 06:35:46 -0700

    Fix issue with noLD testing error.
    Don't run tests when long double not available.

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,3 +1,7 @@
 library(testthat)
 library(smerc)
-test_check("smerc")
+# only test in when long.double available
+# don't have access to non-long.double machine for testing
+if (capabilities("long.double")) {
+  test_check("smerc")
+}
